4a7c529b13aa178b1b7a53dbe80cf7f4b5ad5ea7
MaximCrudu <crudmax@gmail.com>
2023-06-07 11:48:21 -0400

    Creating database constraints by adding the appropriate annotations in java

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -37,11 +37,14 @@ public class Student {
             strategy = GenerationType.SEQUENCE)
     private Long id;
     @NotBlank
+    @Column(nullable = false) // database constraint
     private String name;
     @Email  // can be customized with a regular expression
+    @Column(nullable = false, unique = true)
     private String email;
     @NotNull
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Gender gender;
 
     public Student(String name, String email, Gender gender) {
