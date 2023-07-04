a99f307d94c0c8601df3bb711da2e57d80e69933
MaximCrudu <crudmax@gmail.com>
2023-07-03 23:12:21 -0400

    Test for deploying

--- src/main/java/com/example/demo/info_project/InfoProjectController.java
+++ src/main/java/com/example/demo/info_project/InfoProjectController.java
@@ -1,6 +1,7 @@
 package com.example.demo.info_project;
 
 import lombok.AllArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
